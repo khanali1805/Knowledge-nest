1004b835b26775e61000d18c4f052a4969255b2e
Gulzar Ahmad <cherrycreek61@gmail.com>
2026-08-21 00:06:53 +0500

    Add Monetag service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11554222
+    "zoneId": 11619795
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
